8a7306cc1a1df9ada736b259b50a84d6e9931302
gabrielimartins <martins.gabrieli.07.almeida@outlook.com>
2023-03-20 19:54:49 -0300

    Criando saque na conta

--- src/index.js
+++ src/index.js
@@ -23,6 +23,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
     return next();
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === 'credit') {
+            return acc + operation.amount;
+        }else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+}
+
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
 
@@ -62,7 +74,29 @@ app.post("/deposit", (request, response) => {
         amount,
         created_at: new Date(),
         type: "credit"
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
+app.post("/withdraw", (request, response) => {
+    const { amount } = request.body;
+
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if(balance < amount) {
+        return response.status(400).json({error: "Insufficient funds!"})
     }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit",
+    };
 
     customer.statement.push(statementOperation);
 
