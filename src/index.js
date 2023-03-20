5783457c39a4f3976cfae0217da32ae995510541
gabrielimartins <martins.gabrieli.07.almeida@outlook.com>
2023-03-20 20:24:16 -0300

    Listando o extrato bancario por data

--- src/index.js
+++ src/index.js
@@ -103,4 +103,19 @@ app.post("/withdraw", (request, response) => {
     return response.status(201).send();
 });
 
+app.get("/statement/date", (request, response) => {
+    const { customer } = request;
+    const { date } = request.query;
+
+    const dateFormat = new Date(date + " 00:00");
+
+    const statement = customer.statement.filter(
+        (statement) => 
+            statement.created_at.toDateString() === 
+            new Date(dateFormat).toDateString()
+    );
+    
+    return response.json(statement);
+});
+
 app.listen(3333);
