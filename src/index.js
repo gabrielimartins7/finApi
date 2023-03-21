4270883c81ab9fbcf2088b317e4a1b5562a7ff25
gabrielimartins <martins.gabrieli.07.almeida@outlook.com>
2023-03-21 09:38:23 -0300

    Removar conta

--- src/index.js
+++ src/index.js
@@ -133,4 +133,20 @@ app.get('/account', (request, response) => {
     return response.json(customer);
 });
 
+app.delete('/account', (request, response) => {
+    const { customer } = request;
+
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+});
+
+app.get('/balance', (request, response) => {
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    return response.json(balance);
+});
+
 app.listen(3333);
